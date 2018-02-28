40919dd5f36026be9e103cfc59b0b9fd6838ae96
KJR <kjrkjri@outlook.com>
2018-02-28 16:44:14 +0800

    说明

--- 蚂蚁金服前端笔试题.js
+++ 蚂蚁金服前端笔试题.js
@@ -12,6 +12,7 @@ function reverse(str){
 
 reverse('hello antfin group'); // return 'group antfin hello'
 
+//第一道题很ez 因为之前一直在codewars上做算法练习，这种直接把字符串用空格分开拆分成数组再翻转 再拼接return出来就ok
 
 //2. Please write a add function,
 
@@ -24,3 +25,4 @@ function add(num){
     }
 }
 console.log(add(2)(3)); //print 5 
+// 这道题花了我快一个小时，一开始看见add(2)(3),还以为是不定参数 后来才反应过来应该是闭包 ，而闭包学的不太好，看了看文献才改出来。以后估计就没什么问题了
